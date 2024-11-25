d7a728b408c9cd71bbab6b54e0e9feda270c44ac
reflannuari <reflannuari@gmail.com>
2024-11-25 12:44:45 +0000

    commit

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,26 +1,161 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:myapp/app/controllers/auth_controller.dart';
 
-import '../../../controllers/auth_controller.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  final cAuth = Get.put(AuthController());
+  final cAuth = Get.lazyPut(()=>AuthController());
+  @override
+  Widget build(BuildContext context) {
+    return DashboardAdmin();
+  }
+}
+
+class DashboardAdmin extends StatefulWidget {
+  const DashboardAdmin({super.key});
+
+  @override
+  State<DashboardAdmin> createState() => _DashboardAdminState();
+}
+
+class _DashboardAdminState extends State<DashboardAdmin> {
+  final cAuth = Get.find<AuthController>();
+  int _index = 0;
+  List<Map> _fragment = [
+    {
+      'title': 'Dashboard',
+      'view': 'Dashboard',
+    },
+    {
+      'title': 'Data Mahasiswa',
+      'view': 'Data Mahasiswa',
+    },
+    {
+      'title': 'Data Dosen',
+      'view': 'Data Dosen',
+    },
+    {
+      'title': 'Data Pegawai',
+      'view': 'Data Pegawai',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: drawer(),
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            cAuth.logout();
-          },
-          child: Text("Logout"),
+        backgroundColor: Colors.red,
+        titleSpacing: 0,
+        title: Text(
+          _fragment[_index]['title'],
+          style: TextStyle(
+            color: Colors.white,
+          ),
         ),
+        iconTheme: IconThemeData(
+          color: Colors.white,
+        ),
+      ),
+      //body: _fragment[_index]['view'],
+    );
+  }
+
+  Widget drawer() {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.red,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(
+                  Icons.account_circle,
+                  size: 80,
+                  color: Colors.white,
+                ),
+                Text(
+                  "Reflan Nuari",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                SizedBox(
+                  height: 2,
+                ),
+                Text(
+                  'Admin',
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 0);
+              Get.back();
+            },
+            leading: Icon(Icons.dashboard),
+            title: Text('Dashboard'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.red,
+            textColor: Colors.red,
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 1);
+              Get.back();
+            },
+            leading: Icon(Icons.people),
+            title: Text('Data Mahasiswa'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.red,
+            textColor: Colors.red,
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 2);
+              Get.back();
+            },
+            leading: Icon(Icons.people),
+            title: Text('Data Dosen'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.red,
+            textColor: Colors.red,
+          ),
+          ListTile(
+            onTap: () {
+              setState(() => _index = 3);
+              Get.back();
+            },
+            leading: Icon(Icons.people),
+            title: Text('Data Pegawai'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.red,
+            textColor: Colors.red,
+          ),
+          ListTile(
+            onTap: () {
+              Get.back();
+              cAuth.logout();
+            },
+            leading: Icon(Icons.logout),
+            title: Text('Logout'),
+            trailing: Icon(Icons.navigate_next),
+            iconColor: Colors.red,
+            textColor: Colors.red,
+          ),
+        ],
       ),
     );
   }
